c3e4b568a4f25a552bb1a2eae8e686e273492bbe
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-27 15:39:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string){
 }
 function sayHiToGrandma(spy){
  const lowercase = "I can't hear you"
- If (lowercase.toLowerCase ())=== lowercase{
+ {lowercase.toLowerCase ())=== lowercase{
   return "I can't hear you!"}
 }
