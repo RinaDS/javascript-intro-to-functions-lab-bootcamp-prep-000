866e7839e1e9bf2aac769e054a085612ba18339a
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-27 16:16:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,6 @@ function sayHiToGrandma(string){
   return "I can't hear you!"
 }
 var uppercase = "Yes Indeed"
-if (sayHiToGrandma) === uppercase
+if (sayHiToGrandma) === uppercase {
+  return "Yes Indeed!"
+}
